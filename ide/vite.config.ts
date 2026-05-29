85703e03b043147ff0e3833da59892b68edee848
Mathias Moser <mathias.moser@catena-x.net>
2026-05-29 14:35:47 +0200

    chore(ide): update build configuration

--- ide/vite.config.ts
+++ ide/vite.config.ts
@@ -24,10 +24,16 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   base: "/tractusx-testlab/ide/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   server: {
     port: 5173,
   },
